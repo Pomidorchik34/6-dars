fb570c6c0f0e18a574207e94760a5b34330e0e5e
Pomidorchik34 <dimikabd95@gmail.com>
2024-05-29 08:33:20 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// 1
+// let n = 50;
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i ** 2);
+//   }
+// }
+
+// 2
+// let n = 10;
+// for (let i = 1; i <= n; i++) {
+//   console.log(i ** 3);
+// }
+
+// 3
+
+// 4
+// let n = 30;
+// let sum = 0;
+// for (let i = 0; i <= 30; i++) {
+//   sum = sum + i ** 2;
+// }
+// console.log(sum);
+
+// 5
+// let n = 20;
+// let couunter = 0;
+// for (let i = 1; i <= n; i++) {
+//   for (let k = 1; k <= n; k++) {
+//     if (i % k == 0) {
+//       couunter++;
+//     }
+//   }
+//   if (couunter == 2) {
+//     console.log(i);
+//   }
+// }
+
+// 6
+// for (let i = 1; i <= 100; i++) {
+//   if (i * 2 < 100) {
+//     console.log(i * 2);
+//   }
+// }
+
+// 7
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 === 0 && i % 5 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// 8
+// let n = 24;
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   for (let k = 1; k <= n; k++) {
+//     if (i % k == 0) {
+//       sum = sum + i;
+//     }
+//   }
+// }
+// console.log(sum);
