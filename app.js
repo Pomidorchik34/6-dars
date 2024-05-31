da9b3c9ba2f0a06939916c23981faf24c98afc37
Pomidorchik34 <dimikabd95@gmail.com>
2024-05-31 10:29:43 +0300

    12

--- app.js
+++ app.js
@@ -23,16 +23,15 @@
 // console.log(sum);
 
 // 5
-// let n = 20;
-// let couunter = 0;
-// for (let i = 1; i <= n; i++) {
-//   for (let k = 1; k <= n; k++) {
+// for (let i = 1; i <= 20; i++) {
+//   let couunter = 0;
+//   for (let k = 1; k <= i; k++) {
 //     if (i % k == 0) {
 //       couunter++;
 //     }
-//   }
-//   if (couunter == 2) {
-//     console.log(i);
+//     if (couunter == 2) {
+//       console.log(i);
+//     }
 //   }
 // }
 
@@ -61,3 +60,123 @@
 //   }
 // }
 // console.log(sum);
+
+// 1
+// let a = 70;
+// switch (a) {
+//   case 1:
+//   case 2:
+//   case 12:
+//     console.log(`qish`);
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log(`bohor`);
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log(`yoz`);
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log(`kuz`);
+//     break;
+//   default:
+//     console.log(`hecqaysi emas`);
+//     break;
+// }
+
+// 2
+// switch (a) {
+//   case 90:
+//   case 91:
+//   case 92:
+//   case 93:
+//   case 94:
+//   case 95:
+//   case 96:
+//   case 97:
+//   case 98:
+//   case 99:
+//   case 100:
+//     console.log(`A`);
+//     break;
+//   case 70:
+//   case 71:
+//   case 72:
+//   case 73:
+//   case 74:
+//   case 75:
+//   case 76:
+//   case 77:
+//   case 78:
+//   case 79:
+//   case 80:
+//     console.log(`B`);
+//     break;
+//   case 60:
+//   case 61:
+//   case 62:
+//   case 63:
+//   case 64:
+//   case 65:
+//   case 66:
+//   case 67:
+//   case 68:
+//   case 69:
+//   case 70:
+//     console.log(`B`);
+//     break;
+//   default:
+//     if (a > 100) {
+//       console.log(`A`);
+//     } else if (a < 60) {
+//       console.log(`F`);
+//     }
+// }
+
+// 3
+// function helloMyFreind(time) {
+//   if (time >= 5 && time < 12) {
+//     return "Xayrli tong";
+//   } else if (time >= 12 && time < 17) {
+//     return "Xayrli kun";
+//   } else if (time >= 17 && time < 21) {
+//     return "Xayrli kech";
+//   } else {
+//     return "Xayrli tun";
+//   }
+// }
+// console.log(helloMyFreind(8));
+
+// 4
+// let a = 13;
+// while (a > 12 && a < 12) {
+//   console.log(`not`);
+//   a = +prompt(`password`);
+// }
+// let a = 13;
+// do {
+//   console.log(`not`);
+//   a = +prompt(`password`);
+// } while (a > 12 && a < 12);
+
+// 5
+// let a = 5;
+// while (a <= 20) {
+//   if (a % 2 == 0) {
+//     console.log(a);
+//   }
+//   a++;
+// }
+// do {
+//   if (a % 2 == 0) {
+//     console.log(a);
+//   }
+//   a++;
+// } while (a <= 20);
+
+// 6
